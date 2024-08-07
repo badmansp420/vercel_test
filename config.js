12397b42be9bb9d56694f4e11a0aecd383e7bfe8
PROTIK SARKAR <sarkarprotik240@gmail.com>
2024-08-07 20:43:25 +0530

    added intial

---
+++ config.js
@@ -0,0 +1,9 @@
+import pg from 'pg';
+import 'dotenv/config';
+const { Pool } = pg;
+
+const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL,
+})
+
+export { pool };
---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import 'dotenv/config'
+import { pool } from './config.js';
+const app = express();
+
+// app.get('/', (req, res) => {
+
+//     res.status(200).json({ message: "OK" })
+// })
+
+app.get('/', async (req, res) => {
+    const result = await pool.query('select * from admin');
+    if (!result) {
+        return res.status(404).json({ message: "Network Error" });
+    }
+    res.status(200).json(result.rows);
+})
+
+app.get('/admin', async (req, res) => {
+    const result = await pool.query('select * from admin');
+    if (!result) {
+        return res.status(404).json({ message: "Network Error" });
+    }
+    res.status(200).json(result.rows);
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("Server Start on : " + process.env.PORT);
+});
